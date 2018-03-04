dd9ee51ef87491c05f43235745a65a962301180c
Daniel McNinch <danmcninch@gmail.com>
2018-03-04 12:03:31 -0500

    works on click handlers

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -5,22 +5,22 @@ import './Keypad.css';
 
 const Keypad = ( { pressKey }) => (
     <div className = "keypad">
-        <button className = "calc-button" onClick = { e => pressKey(e, 'clear') }>CLEAR</button>
+        <button className = "calc-button clear-button" onClick = { e => pressKey(e, 'clear') }>CLEAR</button>
         <button className = "calc-button" onClick = { e => pressKey(e, 'slash') }>/</button>
         <button className = "calc-button" onClick = { e => pressKey(e, 7) }>7</button>
         <button className = "calc-button" onClick = { e => pressKey(e, 8) }>8</button>
         <button className = "calc-button" onClick = { e => pressKey(e, 9) }>9</button>
         <button className = "calc-button" onClick = { e => pressKey(e, 'mult') }>*</button>
-        <button className = "calc-button">4</button>
-        <button className = "calc-button">5</button>
-        <button className = "calc-button">6</button>
-        <button className = "calc-button">-</button>
-        <button className = "calc-button">1</button>
-        <button className = "calc-button">2</button>
-        <button className = "calc-button">3</button>
-        <button className = "calc-button">+</button>
-        <button className = "calc-button zero-button">0</button>
-        <button className = "calc-button">=</button>
+        <button className = "calc-button" onClick = { e => pressKey(e, 4) }>4</button>
+        <button className = "calc-button" onClick = { e => pressKey(e, 5) }>5</button>
+        <button className = "calc-button" onClick = { e => pressKey(e, 6) }>6</button>
+        <button className = "calc-button" onClick = { e => pressKey(e, 'minus') }>-</button>
+        <button className = "calc-button" onClick = { e => pressKey(e, 1) }>1</button>
+        <button className = "calc-button" onClick = { e => pressKey(e, 2) }>2</button>
+        <button className = "calc-button" onClick = { e => pressKey(e, 3) }>3</button>
+        <button className = "calc-button" onClick = { e => pressKey(e, 'plus') }>+</button>
+        <button className = "calc-button zero-button" onClick = { e => pressKey(e, 0) }>0</button>
+        <button className = "calc-button" onClick = { e => pressKey(e, 'equals') }>=</button>
     </div>
 );
 
