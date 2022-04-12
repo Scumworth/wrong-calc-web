cbb0fc0c2052e942f7f533ba82111b16f951ccd1
Daniel McNinch <danmcninch@gmail.com>
2022-04-11 21:49:40 -0700

    Updates to remove jumbotron

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,14 @@
 // components/Header.js
 
 import React from 'react';
-import { Jumbotron } from 'react-bootstrap';
 import './Header.css';
 
 const Header = () => (
     <div>
-        <Jumbotron>
+        <div>
             <h1>Wrong Calculator</h1>
             <p>Choose your own calculator experience</p>
-        </Jumbotron>
+        </div>
     </div>
 );
 
